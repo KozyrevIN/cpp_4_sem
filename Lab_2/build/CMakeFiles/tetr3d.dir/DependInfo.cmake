a72c0b0f83a1fb01af22feb8690aa60b81654ba4
KozyrevIN <kozyrev.in@phystech.edu>
2023-02-16 22:16:27 +0300

    added homework 2

---
+++ Lab_2/build/CMakeFiles/tetr3d.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tea_shroom/Programming/cpp_4_sem/Lab_2/tetr3d.cpp" "CMakeFiles/tetr3d.dir/tetr3d.cpp.o" "gcc" "CMakeFiles/tetr3d.dir/tetr3d.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
